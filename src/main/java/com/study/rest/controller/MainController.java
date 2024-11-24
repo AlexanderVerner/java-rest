8c731752d74e7e3e39aefe7d3b59c342078ccb56
Alexander Terentiev <a.terentev@examus.net>
2024-11-24 21:33:30 +0500

    Add swagger for doc goal. Access on ./swagger-ui/index.html

--- src/main/java/com/study/rest/controller/MainController.java
+++ src/main/java/com/study/rest/controller/MainController.java
@@ -3,6 +3,8 @@ package com.study.rest.controller;
 import com.study.rest.DTO.CatDTO;
 import com.study.rest.entity.Cat;
 import com.study.rest.repository.CatRepo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "main_methods")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +20,10 @@ public class MainController {
 
     private final CatRepo catRepo;
 
+    @Operation(
+            summary = "Create new cat in DB",
+            description = "Get DTO cats and build and save entity in database"
+    )
     @PostMapping("/api/add")
     public void addCat(@RequestBody CatDTO catDTO) {
         log.info(
@@ -29,22 +36,37 @@ public class MainController {
         );
     }
 
+    @Operation(
+            summary = "Show all cats in DB",
+            description = "Get list, with all cats in database"
+    )
     @SneakyThrows
     @GetMapping("/api/all")
     public List<Cat> getAllCats() {
         return catRepo.findAll();
     }
 
+    @Operation(
+            summary = "Get by id cat in DB"
+    )
     @GetMapping("/api")
     public Cat getCat(@RequestParam int id) {
         return catRepo.findById(id).orElseThrow();
     }
 
+    @Operation(
+            summary = "Delete cat from DB",
+            description = "Find cat in database by id and delete from it"
+    )
     @DeleteMapping("/api")
     public void deleteCat(@RequestParam int id) {
         catRepo.deleteById(id);
     }
 
+    @Operation(
+            summary = "Put methods for update cat-parameters",
+            description = "Method will check if the cat exists and update it or created a new one"
+    )
     @PutMapping("/api/add")
     public String changeCat(@RequestBody Cat cat) {
         if (!catRepo.existsById(cat.getId())) {
